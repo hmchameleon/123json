57328f3af385915d96eda658cec61c12c5448cb1
Develop <develop@Mac-mini-Sergei.local>
2015-07-31 17:55:20 +0300

    Initial Commit

---
+++ 123json/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  123json
+//
+//  Created by Develop on 31.07.15.
+//  Copyright (c) 2015 Develop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
